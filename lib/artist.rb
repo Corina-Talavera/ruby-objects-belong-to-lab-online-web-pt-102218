83dcdb850570a00a327ae688dfd96037e61ba595
Corina Talavera <corina.talavera@gmail.com>
2018-11-17 17:53:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,19 @@
 class Artist
-  attr_accessor :name
-
-  @@song_count = 0
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
-
-  def songs
-    @songs 
-  end
-
-  def self.song_count
-    @@song_count
-  end
+	attr_accessor :name
+ 	def initialize(name)
+		@name = name
+		@songs = []
+	end
+ 	def add_song(song)
+		@songs << song
+		song.artist = self
+	end
+ 	def songs
+		@songs
+	end
+ 	def genres
+		self.songs.collect do |song|
+			song.genre
+		end
+	end
 end
